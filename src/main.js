d44324be0178871a9ffe0a8992ca9e9b4a755a54
Arka Roy <i.arkaroy@gmail.com>
2018-04-11 23:17:11 +0530

    Added main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+export const is = {
+    arr: a => Array.isArray(a),
+    obj: a => stringContains(Object.prototype.toString.call(a), 'Object'),
+    str: a => typeof a === 'string',
+    fnc: a => typeof a === 'function',
+    und: a => typeof a === 'undefined',
+    num: a => typeof a === 'number'
+};
+
+function stringContains(str, text) {
+    return str.indexOf(text) > -1;
+}
