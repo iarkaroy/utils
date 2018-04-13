f930757a55b01645b8be360ab0e3767823c9420c
Arka Roy <i.arkaroy@gmail.com>
2018-04-13 11:39:20 +0530

    Added more methods and tests

--- src/main.js
+++ src/main.js
@@ -11,3 +11,27 @@ export const is = {
 function stringContains(str, text) {
     return str.indexOf(text) > -1;
 }
+
+export function rand(max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+export function range(end, start = 0, step = 1) {
+    var arr = [];
+    for (var i = start; i < end; i += step) {
+        arr[arr.length] = i;
+    }
+    return arr;
+}
+
+export function max(arr) {
+    return Math.max.apply(Math, arr);
+}
+
+export function min(arr) {
+    return Math.min.apply(Math, arr);
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { is } from '../src/main';
+import { is, range, max, min } from '../src/main';
 
 describe('#is', () => {
 
@@ -49,4 +49,28 @@ describe('#is', () => {
         expect(is.nul(null)).to.equal(true);
     });
 
+});
+
+describe('#range', () => {
+    it('should return range 0 - 5', () => {
+        expect(range(5)).to.deep.equal([0, 1, 2, 3, 4]);
+    });
+    it('should return range 0 - 9 with step 2', () => {
+        expect(range(10, 0, 2)).to.deep.equal([0, 2, 4, 6, 8]);
+    });
+    it('should return range 10 - 100 with step 10', () => {
+        expect(range(100, 10, 10)).to.deep.equal([10, 20, 30, 40, 50, 60, 70, 80, 90]);
+    });
+});
+
+describe('#max', () => {
+    it('should return max', () => {
+        expect(max([0, -5, 100, 374, -498])).to.equal(374);
+    });
+});
+
+describe('#min', () => {
+    it('should return min', () => {
+        expect(min([0, -5, 100, 374, -498])).to.equal(-498);
+    });
 });
